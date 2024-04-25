0caf860c2074e02a0a7ac18cd8e89f90dfcf05c2
Hemang Rami <ramihemang@gmail.com>
2024-04-25 14:10:18 +0530

    Update sample.test.js

--- sample.test.js
+++ sample.test.js
@@ -1,15 +1,1 @@
-const interospectToken = require('./interospectToken'); // assuming interospectToken is defined in a separate file
-
-async function callIntrospections(resultMapTemp, token) {
-    try {
-        const resultMap = await interospectToken(resultMapTemp, token);
-
-        if (resultMap['active'] && resultMap['active'] !== true) {
-            throw new Error(`Invalid token: ${token}`);
-        }
-
-        return resultMap;
-    } catch (error) {
-        throw new Error(error.message);
-    }
-}
+/
