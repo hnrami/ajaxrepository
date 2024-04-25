4921dc49641b36465f5e48c97ae5e30bf8ba83b7
Hemang Rami <ramihemang@gmail.com>
2024-04-25 16:31:44 +0530

    Update sample.test.js

--- sample.test.js
+++ sample.test.js
@@ -1,1 +1,19 @@
-/
+const getIntrospectConfigurations = require('./getIntrospectConfigurations'); // Assuming getIntrospectConfigurations is defined in a separate file
+const verfiyOnLocal = require('./verfiyOnLocal'); // Assuming verfiyOnLocal is defined in a separate file
+const callIntrospections = require('./callIntrospections'); // Assuming callIntrospections is defined in a separate file
+
+async function introspectToken(token, tokenIssuer) {
+    let resultMap = {};
+    let resultMapTemp = {};
+
+    const configMaps = getIntrospectConfigurations();
+    resultMapTemp = configMaps[tokenIssuer];
+
+    if (resultMapTemp['keyset-uri'] !== null) {
+        return await verfiyOnLocal(token, resultMapTemp);
+    }
+
+    resultMap = await callIntrospections(resultMapTemp, token);
+
+    return resultMap;
+}
