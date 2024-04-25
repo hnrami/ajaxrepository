ce0fece4a91ceede50923af8e2080cf4200fcaa3
Hemang Rami <ramihemang@gmail.com>
2024-04-25 14:04:58 +0530

    Update sample.test.js

--- sample.test.js
+++ sample.test.js
@@ -10,3 +10,46 @@ function getAuthorizationHeader(clientID, clientSecret) {
         throw new Error('Could not convert string');
     }
 }
+
+
+const jwt = require('jsonwebtoken');
+const jwkToPem = require('jwk-to-pem');
+const axios = require('axios');
+
+async function verifyOnLocal(token, resultMapTemp) {
+    try {
+        const decodedJwt = jwt.decode(token, { complete: true });
+
+        const myURL = new URL(resultMapTemp['keyset-uri']);
+        const { data: jwkSet } = await axios.get(myURL);
+
+        const jwk = jwkSet.keys.find(key => key.kid === decodedJwt.header.kid);
+        if (!jwk) {
+            throw new Error('Key not found');
+        }
+
+        const pem = jwkToPem(jwk);
+        jwt.verify(token, pem);
+
+        let claimMap = jwt.decode(token, { complete: false });
+
+        if (claimMap.exp && typeof claimMap.exp === 'number') {
+            claimMap.exp = new Date(claimMap.exp * 1000);
+        }
+
+        claimMap['access_token'] = token;
+
+        if (new Date(claimMap.exp) < new Date()) {
+            throw new Error('Expired token!');
+        }
+
+        return claimMap;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
+
+
+
+
+
