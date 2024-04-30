4c646fc1baf114db17f216575b1fc8c9a155262d
Hemang Rami <ramihemang@gmail.com>
2024-04-30 16:01:02 +0530

    Update sample.test.js

--- sample.test.js
+++ sample.test.js
@@ -60,4 +60,58 @@ describe('TokenAuthenticate', () => {
       await expect(tokenAuthenticate.introspectToken(token, tokenIssuer)).to.be.rejectedWith(errorMessage);
     });
   });
+
+
+  const { CONFIGURATIONS } = require('../config/config');
+const constants = require("../constants/constants");
+const TokenAuthenticate = require('../path/to/TokenAuthenticate');
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+
+// Use chai-as-promised plugin
+chai.use(chaiAsPromised);
+
+// Assertion style
+const expect = chai.expect;
+
+// Mocking the dependencies
+const configMock = new Map([
+  ['tokenIssuer1', { [constants.KEYSETURI]: 'someUri1' }],
+  ['tokenIssuer2', { [constants.KEYSETURI]: null }]
+]);
+
+describe('TokenAuthenticate', () => {
+  let tokenAuthenticate;
+
+  beforeEach(() => {
+    tokenAuthenticate = new TokenAuthenticate();
+  });
+
+  describe('tokendecode', () => {
+    it('should call extractTokenIssuer and introspectToken', async () => {
+      const token = 'sampleToken';
+      const tokenIssuer = 'tokenIssuer1';
+      const expectedResult = new Map();
+
+      tokenAuthenticate.extractTokenIssuer = async () => tokenIssuer;
+      tokenAuthenticate.introspectToken = async () => expectedResult;
+
+      const result = await tokenAuthenticate.tokendecode(token);
+
+      expect(result).to.equal(expectedResult);
+    });
+
+    it('should throw an error if any exception occurs', async () => {
+      const token = 'sampleToken';
+      const errorMessage = 'An error occurred';
+
+      tokenAuthenticate.extractTokenIssuer = async () => {
+        throw new Error(errorMessage);
+      };
+
+      await expect(tokenAuthenticate.tokendecode(token)).to.be.rejectedWith(errorMessage);
+    });
+  });
+});
+
 });
