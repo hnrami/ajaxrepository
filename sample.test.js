0d80865a22165b75d1241761c070dbe388d0da80
Hemang Rami <ramihemang@gmail.com>
2024-05-07 15:40:54 +0530

    Update sample.test.js

--- sample.test.js
+++ sample.test.js
@@ -3,24 +3,12 @@ const jwksClient = require('jwks-rsa');
 
 async function verifyOnLocal(token, resultMapTemp) {
     try {
-        // Decode the JWT token
         const decodedJwt = jwt.decode(token, { complete: true });
-
-        // Create a map to store claims
         const claimMap = {};
-
-        // Get the keyset URI from the resultMapTemp
         const keysetUri = resultMapTemp['keyset-uri'];
-
-        // Create a new JWKS (JSON Web Key Set) client instance
-        const client = jwksClient({
-            jwksUri: keysetUri
-        });
-
-        // Get the key ID from the decoded JWT
+        const client = jwksClient({ jwksUri: keysetUri });
         const kid = decodedJwt.header.kid;
 
-        // Retrieve the JWK (JSON Web Key) using the key ID
         const getKey = async () => {
             return new Promise((resolve, reject) => {
                 client.getSigningKey(kid, (err, key) => {
@@ -33,28 +21,21 @@ async function verifyOnLocal(token, resultMapTemp) {
             });
         };
 
-        // Verify the JWT token using the RSA public key fetched from the JWK
-        const decoded = jwt.verify(token, await getKey(), {
-            algorithms: ['RS256']
-        });
+        const publicKey = await getKey();
+        const decoded = jwt.verify(token, publicKey, { algorithms: ['RS256'] });
 
-        // Parse the claims from the token
         for (const [key, value] of Object.entries(decoded)) {
             claimMap[key] = value;
         }
 
-        // If the claimMap contains the 'exp' claim and its value is a number, convert it to a BigInt
         if (claimMap.hasOwnProperty('exp') && typeof claimMap['exp'] === 'number') {
             claimMap['exp'] = BigInt(claimMap['exp']);
         }
 
-        // Add the 'access_token' key to the claimMap with the token value
         claimMap['access_token'] = token;
 
-        // Return the claimMap containing the decoded claims from the token
         return claimMap;
     } catch (error) {
-        // Handle exceptions
         throw new Error(`Error verifying token: ${error.message}`);
     }
 }
